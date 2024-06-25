5b37a15935fe58c1710e43383fa30083efeceef5
xukai <xukai@nandasoft-its.com>
2024-06-25 15:12:31 +0800

    测试makedown语法

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # web-utils-plus
+# Datejs
+```import { Datejs } from web-utils-plus
+const date = new Datejs('2024-08-11').format('YYYY-MM-DD')
+```
+| 字符 | 含义 | 例子 |
+| - | - | - |
+| YYYY | 年份 | 2024 |
+
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import throttle from "./throttle.js"
 import debounce from "./debounce.js"
 import Datejs from './formatter.js'
-const date = new Datejs('2024-06-30').format('dd')
-console.log(date)
 export {
     throttle,
     debounce,
